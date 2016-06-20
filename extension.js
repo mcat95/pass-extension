ba0b2d60fd129fa73dc1c6f887355660243d8e24
Marco <mcat95@gmail.com>
2016-06-20 23:13:54 +0200

    Initial list of directories

--- extension.js
+++ extension.js
@@ -7,26 +7,56 @@ const St = imports.gi.St;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const Util = imports.misc.util;
+const Clutter = imports.gi.Clutter;
+
+
+const SeparatorMenuItem = new Lang.Class({
+  Name: 'SeparatorMenuItem',
+  Extends: PopupMenu.PopupBaseMenuItem,
+  _init: function (text) {
+    this.parent({ reactive: false, can_focus: false});
+    this._separator = new St.Widget({ style_class: 'popup-separator-menu-item',
+                                      y_expand: true,
+                                      y_align: Clutter.ActorAlign.CENTER });
+    this.actor.add(this._separator, { expand: true });
+  },
+});
 
 const PasswordManager = new Lang.Class({
   Name: 'PasswordManager',
   Extends: PanelMenu.Button,
+  _current_directory: './',
 
   _init: function() {
     PanelMenu.Button.prototype._init.call(this, 0.0);
 
     let hbox = new St.BoxLayout({ style_class: 'panel-status-menu-box' });
     let icon = new St.Icon({icon_name: 'system-run-symbolic', style_class: 'system-status-icon'});
+
     hbox.add_child(icon);
-
     this.actor.add_actor(hbox);
-    this.actor.add_style_class_name('panel-status-button');
-
     Main.panel.addToStatusArea('passwordManager', this);
-
-    let item = new PopupMenu.PopupMenuItem("This is a test");
-    this.menu.addMenuItem(item);
+    this._draw_directory();
   },
+
+  _draw_directory: function(){
+    let item = new PopupMenu.PopupMenuItem(this._current_directory);
+    this.menu.addMenuItem(item);
+    this.menu.addMenuItem(new SeparatorMenuItem());
+
+    let cmd = "ls -l .password-store/"+this._current_directory;
+    let [res, out, err, status] = GLib.spawn_command_line_sync(cmd);
+    let data = out.toString().split("\n").slice(1).map(element => ({
+      directory: element[0] === 'd',
+      name: element.split(" ").slice(-1)[0],
+    }));
+    data.forEach(element => {
+      let menuElement = element.directory ?
+        new PopupMenu.PopupSubMenuMenuItem(element.name) :
+        new PopupMenu.PopupMenuItem(element.name);
+      this.menu.addMenuItem(menuElement);
+    });
+  }
 });
 
 let passwordManager;
