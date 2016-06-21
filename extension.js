609f42618ad567ac1d674599ba6a9b1bc921ee0b
Marco <mcat95@gmail.com>
2016-06-21 20:21:07 +0200

    More appropiate icon

--- extension.js
+++ extension.js
@@ -31,7 +31,7 @@ const PasswordManager = new Lang.Class({
     PanelMenu.Button.prototype._init.call(this, 0.0);
 
     let hbox = new St.BoxLayout({ style_class: 'panel-status-menu-box' });
-    let icon = new St.Icon({icon_name: 'system-run-symbolic', style_class: 'system-status-icon'});
+    let icon = new St.Icon({icon_name: 'dialog-password-symbolic', style_class: 'system-status-icon'});
 
     hbox.add_child(icon);
     this.actor.add_actor(hbox);
